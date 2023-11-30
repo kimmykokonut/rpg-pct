67d66dab65445913cee5805198a298368418064a
Kim Robinson <kimmykokonut@gmail.com>
2023-11-30 12:04:01 -0800

    WIP
    Co-authored-by: Monica Barboza <MonBoza@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { experiencedHiker, weekendWarrior, map, stateControl } from './js/rpg.js';
-//changeState, eatingSleepingHikingPerson,
-
+//, eatingSleepingHikingPerson,
+function displayResults(char) {
+    
+}
 
 function handleAvatar(e) {
   e.preventDefault();
@@ -27,26 +29,41 @@ function handleAvatar(e) {
   divIntro.setAttribute("class", "hidden");
   const divSetup = document.getElementById('setup');
   divSetup.removeAttribute("class");
-}
-function handleBackpack(e) {
-  handleAvatar();
-  e.preventDefault();
+
+  //  handleBackpack(char);
+  // }
+  // function handleBackpack(e, char) {
+  //   e.preventDefault();
   const backpack = document.querySelector('#items option:checked').textContent;
-  const displayResults = document.getElementById('displayBackpack');
+  const displayResults2 = document.getElementById('displayBackpack');
+  console.log(backpack);
   if (backpack === 'Map') {
-    const newState = stateControl(map);
+    const newState = map(char); //not working
+    console.log(newState);
     console.log(`NewState value: ${newState.wisdom}`);
-    displayResults.innerText = `
-  Backpack contents: 
-  ${backpack}`;
+    displayResults2.innerText = `
+  Backpack contents:
+  ${backpack}
+  stats:
+  Here are your stats: 
+  Health: ${newState.health}
+  Stamina: ${newState.stamina}
+  Wisdom: ${newState.wisdom}`;  //works
   } else {
     console.log("else made it");
   }
+  //displayResults.innerText = ''; //not a sep event
+  //divSetup.setAttribute("class", "hidden");
+
+  //hide display stats div
+  console.log(char);
   //update displayed stats
 }
 
 
+
 window.addEventListener("load", function () {
-  document.querySelector('form#backpack').addEventListener("submit", handleBackpack);
   document.querySelector('form#nameInput').addEventListener("submit", handleAvatar);
+  document.querySelector('form#backpack').addEventListener("submit", handleAvatar);
+
 });
