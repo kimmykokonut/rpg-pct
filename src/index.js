fdaffb3f7e4e069f164f60fb1ede8b8973284982
Kim Robinson <kimmykokonut@gmail.com>
2023-11-30 11:20:18 -0800

    make backpack choice functional and display
    Co-authored-by: Monica Barboza <MonBoza@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import {  experiencedHiker, weekendWarrior } from './js/rpg.js';
-//changeState, stateControl, eatingSleepingHikingPerson,
+import { experiencedHiker, weekendWarrior, map, stateControl } from './js/rpg.js';
+//changeState, eatingSleepingHikingPerson,
 
 
 function handleAvatar(e) {
@@ -13,27 +13,40 @@ function handleAvatar(e) {
   let char;
   if (avatarSelect === 'Weekend Warrior') {
     char = weekendWarrior(nameInput);
-    displayResults.innerText = `${char.name} You have chosen to be a ${avatarSelect}!
-    Here are your stats: 
-    Health: ${char.health}
-    Stamina: ${char.stamina}
-    Wisdom: ${char.wisdom}`;
   } else {
     char = experiencedHiker(nameInput);
-    displayResults.innerText = `${char.name} You have chosen to be a ${avatarSelect}!
-    Here are your stats: 
-    Health: ${char.health}
-    Stamina: ${char.stamina}
-    Wisdom: ${char.wisdom}`;
   }
 
-//   displayResults.innerText = `${char.name} You have chosen to be a ${avatarSelect}!
-//   Here are your stats: 
-//   Health: ${char.health}
-//   Stamina: ${char.stamina}
-//   Wisdom: ${char.wisdom}`;
+  displayResults.innerText = `${char.name} You have chosen to be a ${avatarSelect}!
+  Here are your stats: 
+  Health: ${char.health}
+  Stamina: ${char.stamina}
+  Wisdom: ${char.wisdom}`;
+
+  const divIntro = document.getElementById('intro');
+  divIntro.setAttribute("class", "hidden");
+  const divSetup = document.getElementById('setup');
+  divSetup.removeAttribute("class");
+}
+function handleBackpack(e) {
+  handleAvatar();
+  e.preventDefault();
+  const backpack = document.querySelector('#items option:checked').textContent;
+  const displayResults = document.getElementById('displayBackpack');
+  if (backpack === 'Map') {
+    const newState = stateControl(map);
+    console.log(`NewState value: ${newState.wisdom}`);
+    displayResults.innerText = `
+  Backpack contents: 
+  ${backpack}`;
+  } else {
+    console.log("else made it");
+  }
+  //update displayed stats
 }
 
+
 window.addEventListener("load", function () {
+  document.querySelector('form#backpack').addEventListener("submit", handleBackpack);
   document.querySelector('form#nameInput').addEventListener("submit", handleAvatar);
 });
